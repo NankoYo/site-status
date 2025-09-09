2bfa5eda63f8b7d8e6b6520c3057448ad437b41b
NanKoYo <a3594399324@gmail.com>
2025-09-10 00:25:32 +0800

    Update SiteFooter.vue

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -50,9 +50,9 @@ const { public: configPublic } = useRuntimeConfig();
 const { siteIcp, version } = configPublic;
 
 const linkData = {
-  github: "https://github.com/imsyy/site-status",
-  home: "https://www.imsyy.top",
-  email: "mailto:one@imsyy.top",
+  github: "https://github.com/NanKoYo",
+  home: "https://www.nakeu.cn",
+  email: "mailto:one@nakeu.cn",
 };
 </script>
 
